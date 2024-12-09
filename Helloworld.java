ee49edc27e0ebebfb9471017dc58d2a8052186c6
bogi <yashaswiboga@gmail.com>
2024-12-09 16:48:26 +0530

    second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,20 @@
 public class Helloworld
 {
-    public static void main(String[] args)
-    {
-        System.out.println("HELLO WORLD");
+   public static void main(String[] args) {
+       System.out.println("Prime numbers between 1 and 100 are:");
+
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    
     }
 }
